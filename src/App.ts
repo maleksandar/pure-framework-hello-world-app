18d791cb25a410fda8d0162ec91bbcfbac2d3037
Aleksandar Milosavljević <alemilosav@gmail.com>
2021-09-17 15:45:31 +0200

    typo fix

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,18 @@
 import { Component, componentFactory, Store } from "pure-framework/core";
 import { div, span } from "pure-framework/html";
 import { AppModel } from "./model";
+import { store } from "./store";
 class AppComponent extends Component<AppModel> {
   template() {
     return div({ class: 'app-root'}, [
       span(`Hello ${this.state.name}! Welcome! `),
-      span(`The answer to the utimate question of life, the universe and everything is: ${this.state.answer}`)
-    ])
+      span(`The answer to the ultimate question of life, the universe and everything is: ${this.state.answer}`)
+    ]).on('click', () => {
+      store.updateState({
+        answer: this.state.answer + 1,
+        name: 'Ford'
+      });
+    });
   }
 }
 
